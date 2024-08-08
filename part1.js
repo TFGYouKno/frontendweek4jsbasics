476b835fc9a03c47c55fa4b89e531d60d6a73974
TFGYouKno <brockhoffharrison@comcast.net>
2024-08-08 18:02:54 -0400

    initial commit

---
+++ part1.js
@@ -0,0 +1,10 @@
+const person = "Harrison";
+let age = 32;
+let grades = {math: 100, science: 95, english: 90, history: 100};
+let isGoodAttendence = true;
+
+
+console.log("Name:", person);
+console.log("Age:", age);
+console.log("Grades", grades);
+console.log("In good attendence standings: ", isGoodAttendence);
---
+++ part2.js
@@ -0,0 +1,29 @@
+let x = 100;
+let y = 20;
+
+let sum = x + y;
+let sub = x - y;
+let product = x * y;
+let quotient = x / y;
+
+console.log("Sum:", sum);
+console.log("Difference:", sub);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+
+let NewNum = 40;
+
+let Updatedsum = sum + NewNum;
+let Updatedsub = sub - NewNum;
+let Updatedproduct = product * NewNum;
+let Updatedquotient = quotient / NewNum;
+
+console.log("Updated Total:", Updatedsum);
+console.log("Updated Total:", Updatedsub);
+console.log("Updated Total:", Updatedproduct);
+console.log("Updated Total:", Updatedquotient);
+
+
+let isEqualto = Updatedproduct == sum
+
+console.log("Is your updated result equal to your original result?", isEqualto);
